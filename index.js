41803905098f19a24a583f056497e85e95d93022
louisogwal <yienlouis470@gmail.com>
2024-09-27 11:03:51 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreenting = `Welcome, ${currentUser.slice(0,1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
